320f3c915ffcd49f23b6831bbe203f22c9dceb89
Kouki Hikita <kouki.hikita@gmail.com>
2017-02-22 16:18:44 +0900

    added JPA Repository

--- src/main/java/com/example/CustomerController.java
+++ src/main/java/com/example/CustomerController.java
@@ -1,7 +1,6 @@
 package com.example;
 
-//import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
-//import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+import java.util.List;
 
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
@@ -9,17 +8,26 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @RestController
 public class CustomerController {
 
     private static final String template = "Hello, %s!";
+    
+    private CustomerRepository repository;
 
-    @GetMapping("/greeting")
+    @Autowired
+    public CustomerController(CustomerRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping("/customer")
     public HttpEntity<Customer> getCustomer(@RequestParam(required=false, defaultValue="Customer") String name) {
         System.out.println("==== in customer ====");
         
-        Customer customer = new Customer("Hello", String.format(template, name));
+        List<Customer> records = repository.findByLastName(name);
+        Customer customer = new Customer("Hello", String.format(template, records.toString()));
         
         return new ResponseEntity<Customer>(customer, HttpStatus.OK);
     }
--- src/main/java/com/example/CustomerRepository.java
+++ src/main/java/com/example/CustomerRepository.java
@@ -2,9 +2,12 @@ package com.example;
 
 import java.util.List;
 
-import org.springframework.data.repository.CrudRepository;
+//import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface CustomerRepository extends CrudRepository<Customer, Long> {
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
 
     List<Customer> findByLastName(String lastName);
 }
